801384610ee875c4d19587c1f4bb61e5c1ffaff4
LimeZzje <43118370+LimeZzje@users.noreply.github.com>
2019-12-30 12:14:12 +0100

    Add files via upload

--- home/trickTutorials/js/index.js
+++ home/trickTutorials/js/index.js
@@ -1,7 +1,29 @@
 $(function(){
-    var treeView = $("#treeview").dxTreeView({ 
-        items:treeViewData,
-        width: 500,
+    var treeView = $("#trickview").dxTreeView({ 
+        items: tricks,
+        width: 300,
+        searchEnabled: true,
+        onItemClick:function(e) {
+            var item = e.itemData;
+            if(item.name){
+                window.open("https://www.youtube.com/results?search_query=" + item.name + "+Penspinning+Tutorial")
+            }
+        }
+    }).dxTreeView("instance");
+
+    $("#searchMode").dxSelectBox({
+        items: ["contains", "startswith"],
+        value: "contains",
+        onValueChanged: function(data) {
+            treeView.option("searchMode", data.value);
+        }
+    });
+});
+
+$(function(){
+    var treeView = $("#linkview").dxTreeView({ 
+        items: linkings,
+        width: 300,
         searchEnabled: true,
         onItemClick:function(e) {
             var item = e.itemData;
